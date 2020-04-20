eb975362705fa808e4915c25531c5ceac8ea7745
rmunro_waracle <r.munro@waracle.com>
2020-04-20 23:53:45 +0100

    try to add button to pause - not working

--- app/src/main/java/uk/co/sentinelweb/cuer/app/service/cast/YoutubeCastService.kt
+++ app/src/main/java/uk/co/sentinelweb/cuer/app/service/cast/YoutubeCastService.kt
@@ -1,6 +1,8 @@
 package uk.co.sentinelweb.cuer.app.service.cast
 
 import android.app.Notification
+import android.app.Notification.EXTRA_NOTIFICATION_ID
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
@@ -8,46 +10,65 @@ import androidx.core.app.NotificationCompat
 import com.roche.mdas.util.wrapper.ToastWrapper
 import org.koin.android.ext.android.inject
 import org.koin.core.KoinComponent
+import org.koin.core.scope.Scope
 import org.koin.ext.getOrCreateScope
 import uk.co.sentinelweb.cuer.app.R
 import uk.co.sentinelweb.cuer.app.util.wrapper.NotificationWrapper
 
 class YoutubeCastService : Service(), KoinComponent {
 
+    private lateinit var scope : Scope
     private lateinit var controller : YoutubeCastServiceController
     private val toastWrapper: ToastWrapper by inject()
     private val notif: NotificationWrapper by inject()
+
     private lateinit var notifChannelId:String // todo move to appstate
 
     override fun onCreate() {
         super.onCreate()
         _instance = this
-        val s = this.getOrCreateScope()
-        controller = s.get()
+        scope = this.getOrCreateScope().apply {
+            controller = get()
+        }
         controller.initialise()
-        toastWrapper.showToast("Service created")
+        // toastWrapper.showToast("Service created")
         notifChannelId = notif.createChannelId()// todo create earlier?
-        startForeground(FOREGROUND_ID,notification().build())
+        startForeground(FOREGROUND_ID,notification())
     }
 
     // todo move to wrapper
-    fun notification() = NotificationCompat.Builder(this, notifChannelId)
-        .setDefaults(Notification.DEFAULT_ALL)
-        .setSmallIcon(R.drawable.ic_player_fast_rewind_black)
-        .setContentTitle("service")
-        .setContentText("content")
-        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-        .setAutoCancel(true)
-        .setWhen(System.currentTimeMillis())
+    fun notification():Notification {
+        val snoozeIntent = Intent(this, YoutubeCastService::class.java).apply {
+            action = ACTION_PAUSE
+            putExtra(EXTRA_NOTIFICATION_ID, 0)
+        }
+        val snoozePendingIntent: PendingIntent =
+            PendingIntent.getService(this, 0, snoozeIntent, 0)
 
+        return NotificationCompat.Builder(this, notifChannelId)
+            .setDefaults(Notification.DEFAULT_ALL)
+            .setSmallIcon(R.drawable.ic_player_fast_rewind_black)
+            .setContentTitle("service")
+            .setContentText("content")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+            .setWhen(System.currentTimeMillis())
+            .addAction(
+                R.drawable.ic_player_pause_black, "Pause",
+                snoozePendingIntent
+            ).build()
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return super.onStartCommand(intent, flags, startId)
+        if (ACTION_PAUSE == intent?.action) {
+            controller.pause()
+        }
+        return START_NOT_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        toastWrapper.showToast("Service destroyed")
+        // toastWrapper.showToast("Service destroyed")
         controller.destroy()
         _instance = null
     }
@@ -56,6 +77,8 @@ class YoutubeCastService : Service(), KoinComponent {
 
     companion object {
         private const val FOREGROUND_ID = 34563
+        private const val NOTIF_ID = 34564
+        private const val ACTION_PAUSE = "pause"
         private var _instance : YoutubeCastService? = null
 
         fun instance() : YoutubeCastService? = _instance
--- app/src/main/java/uk/co/sentinelweb/cuer/app/service/cast/YoutubeCastServiceController.kt
+++ app/src/main/java/uk/co/sentinelweb/cuer/app/service/cast/YoutubeCastServiceController.kt
@@ -3,6 +3,8 @@ package uk.co.sentinelweb.cuer.app.service.cast
 import uk.co.sentinelweb.cuer.app.util.cast.ChromeCastWrapper
 import uk.co.sentinelweb.cuer.app.util.cast.listener.ChromecastYouTubePlayerContextWrapper
 import uk.co.sentinelweb.cuer.app.util.cast.listener.YoutubePlayerContextCreator
+import uk.co.sentinelweb.cuer.app.util.cast.ui.CastPlayerContract
+import java.lang.IllegalStateException
 
 class YoutubeCastServiceController constructor(
     private val service:YoutubeCastService,
@@ -11,11 +13,72 @@ class YoutubeCastServiceController constructor(
 ) {
     private var wrapper: ChromecastYouTubePlayerContextWrapper? = null
 
+    private val control = Control()
+
     fun initialise() {
-        wrapper = creator.createContext(chromeCastWrapper.getCastContext())
+        wrapper = creator.createContext(chromeCastWrapper.getCastContext()).apply {
+            playerUi = control
+        }
+
     }
 
     fun destroy() {
         wrapper = null
+    }
+
+    fun pause() {
+        // doesnt work - may have to retain the one ChromecastYouTubePlayerContextWrapper
+        control.listeners.get(0).pause()
+    }
+
+    inner class Control() :CastPlayerContract.PlayerControls {
+
+        val listeners : MutableList<CastPlayerContract.PlayerControls.Listener> = mutableListOf()
+
+        override fun initMediaRouteButton() {
+
+        }
+
+        override fun setConnectionState(connState: CastPlayerContract.ConnectionState) {
+
+        }
+
+        override fun setPlayerState(playState: CastPlayerContract.PlayerStateUi) {
+
+        }
+
+        override fun addListener(l: CastPlayerContract.PlayerControls.Listener) {
+            if (listeners.size>0) throw IllegalStateException("more than 1 listener in cast player - might not be a problem")
+            listeners.add(l)
+        }
+
+        override fun removeListener(l: CastPlayerContract.PlayerControls.Listener) {
+            listeners.remove(l)
+        }
+
+        override fun setCurrentSecond(second: Float) {
+
+        }
+
+        override fun setDuration(duration: Float) {
+
+        }
+
+        override fun error(msg: String) {
+
+        }
+
+        override fun setTitle(title: String) {
+
+        }
+
+        override fun reset() {
+
+        }
+
+        override fun restoreState() {
+
+        }
+
     }
 }
