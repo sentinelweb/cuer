93c2045aa2b3ad67687096764964e9ceb07d8383
Rob Munro <sentinelwebtechnologies@gmail.com>
2020-06-04 12:45:39 +0100

    make seekbar changeabke in media notif (#64)

--- app/src/main/java/uk/co/sentinelweb/cuer/app/util/mediasession/MediaSessionManager.kt
+++ app/src/main/java/uk/co/sentinelweb/cuer/app/util/mediasession/MediaSessionManager.kt
@@ -3,6 +3,7 @@ package uk.co.sentinelweb.cuer.app.util.mediasession
 import android.content.Context
 import android.support.v4.media.session.MediaSessionCompat
 import uk.co.sentinelweb.cuer.app.CuerAppState
+import uk.co.sentinelweb.cuer.app.ui.play_control.CastPlayerContract
 import uk.co.sentinelweb.cuer.core.wrapper.LogWrapper
 import uk.co.sentinelweb.cuer.domain.MediaDomain
 import uk.co.sentinelweb.cuer.domain.PlayerStateDomain
@@ -18,11 +19,11 @@ class MediaSessionManager constructor(
         log.tag = this::class.java.simpleName
     }
 
-    fun createMediaSession() {
+    fun createMediaSession(controls: CastPlayerContract.PlayerControls.Listener) {
         if (appState.mediaSession == null) {
             appState.mediaSession = MediaSessionCompat(context, "CuerService")
                 .apply {
-                    setCallback(CuerMediaSessionCallback())
+                    setCallback(CuerMediaSessionCallback(controls))
                     isActive = true
                 }
         }
@@ -42,34 +43,47 @@ class MediaSessionManager constructor(
     }
 
     // todo this will go somewhere near the player controls
-    inner class CuerMediaSessionCallback : MediaSessionCompat.Callback() {
+    inner class CuerMediaSessionCallback(private val controls: CastPlayerContract.PlayerControls.Listener) :
+        MediaSessionCompat.Callback() {
 
         override fun onPlay() {
+            controls.play()
             log.d("onPlay")
         }
 
         override fun onPause() {
+            controls.pause()
             log.d("onPause")
         }
 
-        override fun onRewind() {
-            log.d("onRewind")
-        }
-
         override fun onSkipToPrevious() {
+            controls.trackBack()
             log.d("onSkipToPrevious")
         }
 
-        override fun onFastForward() {
-            log.d("onFastForward")
-        }
-
         override fun onStop() {
+            controls.pause()
             log.d("onStop")
         }
 
         override fun onSkipToNext() {
+            controls.trackFwd()
             log.d("onSkipToNext")
+        }
+
+        override fun onSeekTo(pos: Long) {
+            controls.seekTo(pos)
+            log.d("onSeekTo: $pos")
+        }
+
+        override fun onRewind() {
+            // todo add methods in interface (if needed)
+            log.d("onRewind")
+        }
+
+        override fun onFastForward() {
+            // todo add methods in interface (if needed)
+            log.d("onFastForward")
         }
     }
 }
