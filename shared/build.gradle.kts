29a0b8fdea3e40eef30e5539bc3ecd53610db98e
sentinelweb <sentinelwebtechnologies@gmail.com>
2021-05-24 19:38:27 +0200

    make shared module build

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -7,6 +7,24 @@ plugins {
 
 version = "1.0"
 
+android {
+    compileSdkVersion(30)
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdkVersion(16)
+        targetSdkVersion(30)
+    }
+    // remove when upgrading to kotlin 1.5
+    configurations {
+        create("androidTestApi")
+        create("androidTestDebugApi")
+        create("androidTestReleaseApi")
+        create("testApi")
+        create("testDebugApi")
+        create("testReleaseApi")
+    }
+}
+
 kotlin {
     jvm()
     js {
@@ -16,17 +34,13 @@ kotlin {
 
     sourceSets {
         val commonMain by getting
+        val commonTest by getting
         val androidMain by getting
+        val androidTest by getting
         val jsMain by getting
+        val jsTest by getting
         val jvmMain by getting
+        val jvmTest by getting
     }
 }
 
-android {
-    compileSdkVersion(30)
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    defaultConfig {
-        minSdkVersion(16)
-        targetSdkVersion(30)
-    }
-}
