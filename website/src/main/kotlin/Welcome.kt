c615a4f58fe9bbedf6a93b1e7ba850d6fc911a85
sentinelweb <sentinelwebtechnologies@gmail.com>
2021-07-07 12:51:37 +0200

    make module and add site

--- website/src/main/kotlin/Welcome.kt
+++ website/src/main/kotlin/Welcome.kt
@@ -1,9 +1,13 @@
-
+import kotlinx.html.InputType
+import kotlinx.html.js.onChangeFunction
+import org.w3c.dom.HTMLInputElement
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
+import react.dom.attrs
 import react.dom.div
+import react.dom.input
 
 external interface WelcomeProps : RProps {
     var name: String
@@ -21,6 +25,17 @@ class Welcome(props: WelcomeProps) : RComponent<WelcomeProps, WelcomeState>(prop
     override fun RBuilder.render() {
         div {
             +"Hello, ${state.name}"
+        }
+        input {
+            attrs {
+                type = InputType.text
+                value = state.name
+                onChangeFunction = { event ->
+                    setState(
+                        WelcomeState(name = (event.target as HTMLInputElement).value)
+                    )
+                }
+            }
         }
     }
 }
