1621e39147e100ca424a2da80405a2a88b309909
rob_munro <rrmunro@gmail.com>
2022-07-27 10:05:07 +0200

    253 fix channel not launching PlaylistItemEdit

--- app/src/main/java/uk/co/sentinelweb/cuer/app/util/wrapper/UrlLauncherWrapper.kt
+++ app/src/main/java/uk/co/sentinelweb/cuer/app/util/wrapper/UrlLauncherWrapper.kt
@@ -5,14 +5,13 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.content.Intent.ACTION_VIEW
 import android.net.Uri
-import com.google.android.youtube.player.YouTubeIntents
-import uk.co.sentinelweb.cuer.domain.MediaDomain
 
 class UrlLauncherWrapper(private val activity: Activity) {
 
     fun launchUrl(url: String): Boolean = try {
         val parse = Uri.parse(url)
-        activity.startActivity(Intent(ACTION_VIEW, parse))
+        val intent = Intent(ACTION_VIEW, parse)
+        activity.startActivity(intent)
         true
     } catch (e: ActivityNotFoundException) {
         false
