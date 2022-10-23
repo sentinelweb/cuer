ba98ec3bc748375df352af8ed44a1690e65fdb31
rob_munro <rrmunro@gmail.com>
2022-10-23 19:31:11 +0200

    200 fade in and out chips as they load

--- app/src/main/java/uk/co/sentinelweb/cuer/app/ui/browse/BrowseComposables.kt
+++ app/src/main/java/uk/co/sentinelweb/cuer/app/ui/browse/BrowseComposables.kt
@@ -1,10 +1,13 @@
 package uk.co.sentinelweb.cuer.app.ui.browse
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,6 +17,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.arkivanov.mvikotlin.core.view.BaseMviView
+import kotlinx.coroutines.delay
 import uk.co.sentinelweb.cuer.app.R
 import uk.co.sentinelweb.cuer.app.ui.browse.BrowseContract.Order.A_TO_Z
 import uk.co.sentinelweb.cuer.app.ui.browse.BrowseContract.Order.CATEGORIES
@@ -36,11 +40,13 @@ object BrowseComposables {
 //    }
 
     @Composable
+    @ExperimentalAnimationApi
     fun BrowseUi(view: BrowseMviView) {
         BrowseView(view.observableModel, view)
     }
 
     @Composable
+    @ExperimentalAnimationApi
     fun BrowseView(model: Model, view: BaseMviView<Model, Event>) {
         CuerBrowseTheme {
             Surface {
@@ -89,34 +95,55 @@ object BrowseComposables {
     }
 
     @Composable
-    private fun CategoryWithTitle(model: CategoryModel, view: BaseMviView<Model, Event>, rows: Int) {
-        Column(modifier = Modifier.fillMaxWidth()) {
-            Text(
-                text = model.title,
-                style = MaterialTheme.typography.h4,
-                color = Color.White,
+    @ExperimentalAnimationApi
+    private fun CategoryWithTitle(
+        model: CategoryModel,
+        view: BaseMviView<Model, Event>,
+        rows: Int
+    ) {
+        val visible = remember { mutableStateOf(false) }
+        LaunchedEffect(Unit) {
+            delay(100)
+            visible.value = true
+        }
+        AnimatedVisibility(visible = visible.value)
+        {
+            Column(
                 modifier = Modifier
-                    .padding(start = dimensionResource(R.dimen.page_margin))
-                    .clickable(onClick = { view.dispatch(OnCategoryClicked(model)) })
-
-            )
-            model.description?.let {
+                    .fillMaxWidth()
+                    .animateEnterExit(
+                        enter = fadeIn(animationSpec = tween(durationMillis = 1000)),
+                        exit = fadeOut(animationSpec = tween(durationMillis = 200))
+                    ),
+            ) {
                 Text(
-                    text = it,
-                    style = MaterialTheme.typography.body1,
-                    color = Color.White
+                    text = model.title,
+                    style = MaterialTheme.typography.h4,
+                    color = Color.White,
+                    modifier = Modifier
+                        .padding(start = dimensionResource(R.dimen.page_margin))
+                        .clickable(onClick = { view.dispatch(OnCategoryClicked(model)) })
+
                 )
+                model.description?.let {
+                    Text(
+                        text = it,
+                        style = MaterialTheme.typography.body1,
+                        color = Color.White
+                    )
+                }
+                Divider(
+                    color = Color.White, modifier = Modifier
+                        .padding(bottom = 8.dp)
+                        .padding(start = dimensionResource(R.dimen.page_margin))
+                )
+                CategoryGrid(rows, model.subCategories, view)
             }
-            Divider(
-                color = Color.White, modifier = Modifier
-                    .padding(bottom = 8.dp)
-                    .padding(start = dimensionResource(R.dimen.page_margin))
-            )
-            CategoryGrid(rows, model.subCategories, view)
         }
     }
 
     @Composable
+    @ExperimentalAnimationApi
     private fun CategoryGrid(
         rows: Int,
         list: List<CategoryModel>,
@@ -124,106 +151,132 @@ object BrowseComposables {
     ) {
         StaggeredGrid(
             rows = rows,
+            count = list.size,
             modifier = Modifier
                 .horizontalScroll(rememberScrollState())
                 .padding(horizontal = dimensionResource(R.dimen.page_margin))
                 .padding(bottom = dimensionResource(R.dimen.page_margin))
         ) {
-            list.forEach {
-                CatChip(it, view)
+            list.forEachIndexed { i, item ->
+                CatChip(item, i, view)
             }
         }
     }
 
     @Composable
+    @ExperimentalAnimationApi
     fun CatChip(
         category: CategoryModel,
+        seq: Int = 0,
         view: BaseMviView<Model, Event>,
     ) {
-        Surface(
-            modifier = Modifier.padding(4.dp),
-            elevation = 4.dp,
-            shape = MaterialTheme.shapes.medium
+        // cos compose is old cant use by
+        val visible = remember { mutableStateOf(false) }
+        LaunchedEffect(Unit) {
+            delay(seq * 25L)
+            visible.value = true
+        }
+        AnimatedVisibility(
+            visible = visible.value,
+            enter = slideInHorizontally(
+                // Enters by sliding down from offset -fullHeight to 0.
+                initialOffsetX = { fullWidth -> -fullWidth },
+                animationSpec = tween(durationMillis = 500, easing = LinearOutSlowInEasing)
+            ),
+            exit = slideOutHorizontally(
+                // Exits by sliding up from offset 0 to -fullHeight.
+                targetOffsetX = { fullWidth -> -fullWidth },
+                animationSpec = tween(durationMillis = 250, easing = FastOutLinearInEasing)
+            )
         ) {
-
-            Row(
-                modifier = Modifier
-                    .clickable(onClick = { view.dispatch(OnCategoryClicked(category)) })
+            Surface(
+                modifier = Modifier.padding(4.dp)
+                    .animateEnterExit(
+                        enter = fadeIn(animationSpec = tween(durationMillis = 300)),
+                        exit = fadeOut(animationSpec = tween(durationMillis = 200))
+                    ),
+                elevation = 4.dp,
+                shape = MaterialTheme.shapes.medium
             ) {
-                Box {
-                    category.thumbNailUrl?.let {
-                        NetworkImage(
-                            url = it,
+                Row(
+                    modifier = Modifier
+                        .clickable(onClick = { view.dispatch(OnCategoryClicked(category)) })
+                ) {
+                    Box {
+                        category.thumbNailUrl?.let {
+                            NetworkImage(
+                                url = it,
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .size(width = 72.dp, height = 72.dp)
+                                    .aspectRatio(1f)
+                            )
+                        } ?: Icon(
+                            painter = painterResource(R.drawable.ic_category),
                             contentDescription = null,
                             modifier = Modifier
-                                .size(width = 72.dp, height = 72.dp)
-                                .aspectRatio(1f)
+                                .size(72.dp)
+                                .padding(start = 24.dp)
+                                .align(Alignment.Center)
                         )
-                    } ?: Icon(
-                        painter = painterResource(R.drawable.ic_category),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(72.dp)
-                            .padding(start = 24.dp)
-                            .align(Alignment.Center)
-                    )
-                }
-                Column {
-                    Text(
-                        text = category.title,
-                        style = MaterialTheme.typography.body1,
-                        modifier = Modifier.padding(
-                            start = 16.dp,
-                            top = 16.dp,
-                            end = 16.dp,
-                            bottom = 8.dp
+                    }
+                    Column {
+                        Text(
+                            text = category.title,
+                            style = MaterialTheme.typography.body1,
+                            modifier = Modifier.padding(
+                                start = 16.dp,
+                                top = 16.dp,
+                                end = 16.dp,
+                                bottom = 8.dp
+                            )
                         )
-                    )
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                            if (category.subCount > 0) {
-                                Icon(
-                                    painter = painterResource(R.drawable.ic_tree_24),
-                                    contentDescription = null,
-                                    modifier = Modifier
-                                        .padding(start = 16.dp)
-                                        .size(16.dp)
-                                )
-                                Text(
-                                    text = category.subCount.toString(),
-                                    style = MaterialTheme.typography.caption,
-                                    modifier = Modifier.padding(start = 8.dp)
-                                )
-                            } else if (category.isPlaylist) {
-                                Icon(
-                                    painter = painterResource(R.drawable.ic_playlist_black),
-                                    contentDescription = null,
-                                    modifier = Modifier
-                                        .padding(start = 16.dp)
-                                        .size(16.dp)
-                                )
-                                if (category.existingPlaylist != null) {
-                                    Text(
-                                        text = category.existingPlaylist
-                                            ?.let { "${it.first.currentIndex} / ${it.second.itemCount}" }
-                                            ?: "",
-                                        style = MaterialTheme.typography.caption,
-                                        modifier = Modifier.padding(end = 8.dp)
-                                    )
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                                if (category.subCount > 0) {
                                     Icon(
-                                        painter = painterResource(R.drawable.ic_visibility_24),
+                                        painter = painterResource(R.drawable.ic_tree_24),
                                         contentDescription = null,
                                         modifier = Modifier
-                                            .padding(end = 4.dp)
+                                            .padding(start = 16.dp)
                                             .size(16.dp)
                                     )
                                     Text(
-                                        text = category.existingPlaylist
-                                            ?.let { "${it.second.watchedItemCount}" }
-                                            ?: "",
+                                        text = category.subCount.toString(),
                                         style = MaterialTheme.typography.caption,
-                                        modifier = Modifier.padding(end = 16.dp)
+                                        modifier = Modifier.padding(start = 8.dp)
                                     )
+                                } else if (category.isPlaylist) {
+                                    Icon(
+                                        painter = painterResource(R.drawable.ic_playlist_black),
+                                        contentDescription = null,
+                                        modifier = Modifier
+                                            .padding(start = 16.dp)
+                                            .size(16.dp)
+                                    )
+                                    if (category.existingPlaylist != null) {
+                                        Text(
+                                            text = category.existingPlaylist
+                                                ?.let { "${it.first.currentIndex} / ${it.second.itemCount}" }
+                                                ?: "",
+                                            style = MaterialTheme.typography.caption,
+                                            modifier = Modifier.padding(end = 8.dp)
+                                        )
+                                        Icon(
+                                            painter = painterResource(R.drawable.ic_visibility_24),
+                                            contentDescription = null,
+                                            modifier = Modifier
+                                                .padding(end = 4.dp)
+                                                .size(16.dp)
+                                        )
+                                        Text(
+                                            text = category.existingPlaylist
+                                                ?.let { "${it.second.watchedItemCount}" }
+                                                ?: "",
+                                            style = MaterialTheme.typography.caption,
+                                            modifier = Modifier.padding(end = 16.dp)
+                                        )
+                                    }
                                 }
                             }
                         }
@@ -231,7 +284,6 @@ object BrowseComposables {
                 }
             }
         }
-
     }
 
     // taken from OWL
@@ -239,6 +291,7 @@ object BrowseComposables {
     private fun StaggeredGrid(
         modifier: Modifier = Modifier,
         rows: Int = 3,
+        count: Int,
         content: @Composable () -> Unit,
     ) {
 
@@ -250,7 +303,7 @@ object BrowseComposables {
             val rowHeights = IntArray(rows) { 0 } // Keep track of the height of each row
 
             // Don't constrain child views further, measure them with given constraints
-            val cols = measurables.size / rows + if (measurables.size % rows > 0) 1 else 0
+            val cols = /*measurables.size*/count / rows + if (count % rows > 0) 1 else 0
             val placeables = measurables.mapIndexed { index, measurable ->
                 val placeable = measurable.measure(constraints)
 
@@ -298,6 +351,7 @@ class TestStrings : BrowseContract.Strings {
 
 @Preview(name = "Top level")
 @Composable
+@ExperimentalAnimationApi
 private fun BrowsePreview() {
     val browseModelMapper = BrowseModelMapper(TestStrings(), AndroidLogWrapper())
     val view = object : BaseMviView<Model, Event>() {}
