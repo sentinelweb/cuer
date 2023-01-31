20a1581d95fc9fe6a00f6af51ece4fa6f3a3e03b
Rob Munro <sentinelwebtechnologies@gmail.com>
2023-01-31 20:48:59 +0100

    Bugfix/405 update items order (#412)
    * 405 test descending items
    * 405 test ascending items

---
+++ shared/src/androidTest/kotlin/uk/co/sentinelweb/cuer/app/usecase/PlaylistUpdateUsecaseTest.kt
@@ -0,0 +1,227 @@
+package uk.co.sentinelweb.cuer.app.usecase
+
+import com.appmattus.kotlinfixture.decorator.nullability.NeverNullStrategy
+import com.appmattus.kotlinfixture.decorator.nullability.nullabilityStrategy
+import com.appmattus.kotlinfixture.kotlinFixture
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import uk.co.sentinelweb.cuer.app.orchestrator.*
+import uk.co.sentinelweb.cuer.app.orchestrator.OrchestratorContract.Filter.PlatformIdListFilter
+import uk.co.sentinelweb.cuer.app.orchestrator.OrchestratorContract.Source
+import uk.co.sentinelweb.cuer.app.orchestrator.OrchestratorContract.Source.LOCAL
+import uk.co.sentinelweb.cuer.app.usecase.PlaylistUpdateUsecase.Companion.SECONDS
+import uk.co.sentinelweb.cuer.core.providers.TimeProvider
+import uk.co.sentinelweb.cuer.core.wrapper.SystemLogWrapper
+import uk.co.sentinelweb.cuer.domain.PlatformDomain
+import uk.co.sentinelweb.cuer.domain.PlaylistDomain
+import uk.co.sentinelweb.cuer.domain.PlaylistDomain.PlaylistTypeDomain.PLATFORM
+import uk.co.sentinelweb.cuer.domain.PlaylistDomain.PlaylistTypeDomain.USER
+import uk.co.sentinelweb.cuer.domain.PlaylistItemDomain
+import kotlin.time.Duration.Companion.seconds
+
+class PlaylistUpdateUsecaseTest {
+    private val fixture = kotlinFixture {
+        nullabilityStrategy(NeverNullStrategy)
+        repeatCount { 6 }
+        // repeatCount(PlaylistDomain::items) { 7 }
+    }
+
+    @MockK
+    lateinit var playlistOrchestrator: PlaylistOrchestrator
+
+    @MockK
+    lateinit var playlistItemOrchestrator: PlaylistItemOrchestrator
+
+    @MockK
+    lateinit var mediaOrchestrator: MediaOrchestrator
+
+    @MockK
+    lateinit var playlistMediaLookupUsecase: PlaylistMediaLookupUsecase
+
+    @MockK
+    lateinit var timeProvider: TimeProvider
+
+    @MockK
+    lateinit var updateChecker: PlaylistUpdateUsecase.UpdateCheck
+
+    private val log = SystemLogWrapper()
+    private val timeProviderTest: TimeProvider = TimeProvider()
+
+    private lateinit var sut: PlaylistUpdateUsecase
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this, relaxed = true)
+        sut = PlaylistUpdateUsecase(
+            playlistOrchestrator,
+            playlistItemOrchestrator,
+            mediaOrchestrator,
+            playlistMediaLookupUsecase,
+            timeProvider,
+            log,
+            updateChecker
+        )
+    }
+
+    @Test
+    fun checkToUpdate_true() {
+        every { updateChecker.shouldUpdate(any()) } returns true
+
+        assertTrue(updateChecker.shouldUpdate(fixture()))
+    }
+
+    @Test
+    fun checkToUpdate_false() {
+        every { updateChecker.shouldUpdate(any()) } returns false
+
+        assertFalse(updateChecker.shouldUpdate(fixture()))
+    }
+
+    @Test
+    fun update_must_be_platform() = runTest {
+        val playlist = generatePlaylist().copy(type = USER)
+
+        val actual = sut.update(playlist)
+
+        assertFalse(actual.success)
+    }
+
+    @Test
+    fun update_descending_playlist_must_add_at_top_in_reverse_order() = runTest {
+        val platformId = "update_platformId"
+        val tz = TimeZone.UTC
+        val localtime = timeProviderTest.localDateTime()
+        val existingPlaylist = generatePlaylist().let { pl ->
+            pl.copy(
+                id = 1,
+                type = PLATFORM,
+                platform = PlatformDomain.YOUTUBE,
+                platformId = platformId,
+                items = pl.items.mapIndexed { i, item ->
+                    item.copy(
+                        order = (pl.items.size - i + 7) * SECONDS,
+                        media = item.media.copy(
+                            published = localtime.toInstant(tz).plus(-i.seconds).toLocalDateTime(tz)
+                        )
+                    )
+                }
+            )
+        }
+        //log.d("existingPlaylist: ${existingPlaylist.items.size}")
+        val updatedPlatformPlaylist = generatePlaylist().let { pl ->
+            pl.copy(
+                id = null,
+                type = PLATFORM,
+                platform = PlatformDomain.YOUTUBE,
+                platformId = platformId,
+                items = existingPlaylist.items.plus(pl.items)
+            )
+        }
+        //log.d("updatedPlatformPlaylist: ${updatedPlatformPlaylist.items.size}")
+        //log.d(updatedPlatformPlaylist.items.map { it.media.platformId }.joinToString(","))
+
+        coEvery {
+            playlistOrchestrator.loadByPlatformId(platformId, Source.PLATFORM.deepOptions())
+        } returns updatedPlatformPlaylist
+
+        coEvery {
+            mediaOrchestrator.loadList(
+                PlatformIdListFilter(updatedPlatformPlaylist.items.map { it.media.platformId }),
+                LOCAL.flatOptions()
+            )
+        } returns existingPlaylist.items.map { it.media }
+
+        coEvery { playlistMediaLookupUsecase.lookupMediaAndReplace(any()) } answers { firstArg() }
+        coEvery {
+            playlistItemOrchestrator.save(any<List<PlaylistItemDomain>>(), LOCAL.deepOptions())
+        } answers { firstArg() }
+
+        val actual = sut.update(existingPlaylist)
+
+        //log.d("actual; ${actual.success} ${actual.newItems?.size}")
+        assertTrue(actual.success)
+        assertEquals(6, actual.newItems?.size)
+        // existing order is:: minOrder: 8000 maxOrder: 13000 orderIsAscending: false
+        // so least recent is 7000 and most recent is 2000
+        actual.newItems?.forEachIndexed { i, item ->
+            assertEquals(8 * SECONDS - ((6 - i) * SECONDS), item.order)
+        }
+    }
+
+    @Test
+    fun update_ascending_playlist_must_add_at_bottom_in_order() = runTest {
+        val platformId = "update_platformId"
+        val tz = TimeZone.UTC
+        val localtime = timeProviderTest.localDateTime()
+        val existingPlaylist = generatePlaylist().let { pl ->
+            pl.copy(
+                id = 1,
+                type = PLATFORM,
+                platform = PlatformDomain.YOUTUBE,
+                platformId = platformId,
+                //items = pl.items.mapIndexed { i, item -> item.copy(order = i * 1000L) }
+                items = pl.items.mapIndexed { i, item ->
+                    item.copy(
+                        order = i * SECONDS,
+                        media = item.media.copy(published = localtime.toInstant(tz).plus(i.seconds).toLocalDateTime(tz))
+                    )
+                }
+            )
+        }
+        //log.d("existingPlaylist: ${existingPlaylist.items.size}")
+        val updatedPlatformPlaylist = generatePlaylist().let { pl ->
+            pl.copy(
+                id = null,
+                type = PLATFORM,
+                platform = PlatformDomain.YOUTUBE,
+                platformId = platformId,
+                items = existingPlaylist.items.plus(pl.items)
+            )
+        }
+        //log.d("updatedPlatformPlaylist: ${updatedPlatformPlaylist.items.size}")
+        //log.d(updatedPlatformPlaylist.items.map { it.media.platformId }.joinToString(","))
+
+        coEvery {
+            playlistOrchestrator.loadByPlatformId(platformId, Source.PLATFORM.deepOptions())
+        } returns updatedPlatformPlaylist
+
+        coEvery {
+            mediaOrchestrator.loadList(
+                PlatformIdListFilter(updatedPlatformPlaylist.items.map { it.media.platformId }),
+                LOCAL.flatOptions()
+            )
+        } returns existingPlaylist.items.map { it.media }
+
+        coEvery { playlistMediaLookupUsecase.lookupMediaAndReplace(any()) } answers { firstArg() }
+        coEvery {
+            playlistItemOrchestrator.save(any<List<PlaylistItemDomain>>(), LOCAL.deepOptions())
+        } answers { firstArg() }
+
+        val actual = sut.update(existingPlaylist)
+
+        //log.d("actual; ${actual.success} ${actual.newItems?.size}")
+        assertTrue(actual.success)
+        assertEquals(6, actual.newItems?.size)
+        // existing order is:: minOrder: 8000 maxOrder: 13000 orderIsAscending: false
+        // so least recent is 7000 and most recent is 2000
+        actual.newItems?.forEachIndexed { i, item ->
+            assertEquals(5 * SECONDS + ((i + 1) * SECONDS), item.order)
+        }
+    }
+
+    // sometimes items is empty - which messes up the test
+    private fun generatePlaylist(): PlaylistDomain {
+        var fixPlaylist = fixture<PlaylistDomain>()
+        while (fixPlaylist.items.size == 0) fixPlaylist = fixture()
+        return fixPlaylist
+    }
+}
--- shared/src/commonMain/kotlin/uk/co/sentinelweb/cuer/app/usecase/PlaylistUpdateUsecase.kt
+++ shared/src/commonMain/kotlin/uk/co/sentinelweb/cuer/app/usecase/PlaylistUpdateUsecase.kt
@@ -9,7 +9,9 @@ import uk.co.sentinelweb.cuer.core.wrapper.LogWrapper
 import uk.co.sentinelweb.cuer.domain.PlatformDomain.YOUTUBE
 import uk.co.sentinelweb.cuer.domain.PlaylistDomain
 import uk.co.sentinelweb.cuer.domain.PlaylistDomain.PlaylistTypeDomain.PLATFORM
+import uk.co.sentinelweb.cuer.domain.PlaylistItemDomain
 import uk.co.sentinelweb.cuer.domain.ext.orderIsAscending
+import uk.co.sentinelweb.cuer.domain.ext.summarise
 
 class PlaylistUpdateUsecase constructor(
     private val playlistOrchestrator: PlaylistOrchestrator,
@@ -28,7 +30,8 @@ class PlaylistUpdateUsecase constructor(
     data class UpdateResult(
         val success: Boolean,
         val reason: String = "",
-        val numberItems: Int = -1
+        val numberItems: Int = -1,
+        val newItems: List<PlaylistItemDomain>? = null
     )
 
     fun checkToUpdate(p: PlaylistDomain): Boolean = updateChecker.shouldUpdate(p)
@@ -51,38 +54,45 @@ class PlaylistUpdateUsecase constructor(
                                 ?.takeIf { it.items.size > 0 }
                                 ?.let { playlistMediaLookupUsecase.lookupMediaAndReplace(it) }
                                 ?.let { playlistItemOrchestrator.save(it.items, LOCAL.deepOptions()) }
-                                ?.let { UpdateResult(true, numberItems = it.size) }
+                                ?.let { UpdateResult(true, numberItems = it.size, newItems = it) }
                                 ?: UpdateResult(true, numberItems = 0)
                         }
                         ?: UpdateResult(false, reason = "No platform id")
 
-                    else -> UpdateResult(false, reason = "No platform type")
+                    else -> UpdateResult(false, reason = "Unsupported platform type")
                 }
             }
-            ?: UpdateResult(false, reason = "Not platform type")
+            ?: UpdateResult(false, reason = "Not of PLATFORM type")
     }
 
     private suspend fun removeExistingItems(platform: PlaylistDomain, existing: PlaylistDomain): PlaylistDomain {
+
         val platformPlaylistExistingMediaPlatformIds =
             mediaOrchestrator.loadList(
                 PlatformIdListFilter(platform.items.map { it.media.platformId }),
                 LOCAL.flatOptions()
-            )
-                .map { it.platformId }
+            ).map { it.platformId }
+        //log.d("platformPlaylistExistingMediaPlatformIds: ${platformPlaylistExistingMediaPlatformIds.size}")
+
         val minOrder = if (existing.items.size > 0) existing.items.minOf { it.order } else 0
         val maxOrder = if (existing.items.size > 0) existing.items.maxOf { it.order } else 0
         val orderIsAscending = existing.orderIsAscending()
-        val newItems =
-            platform.items.toMutableList()
-                .apply { removeAll { platformPlaylistExistingMediaPlatformIds.contains(it.media.platformId) } }
-        return existing.copy(items = newItems.mapIndexed { i, item ->
+
+        log.d("minOrder: $minOrder maxOrder: $maxOrder orderIsAscending: $orderIsAscending")
+        val newItems = platform.items.toMutableList()
+            .apply { removeAll { platformPlaylistExistingMediaPlatformIds.contains(it.media.platformId) } }
+
+        val playlistWithNewItemsOnly = existing.copy(items = newItems.mapIndexed { i, item ->
             item.copy(
                 id = null,
                 playlistId = existing.id,
                 dateAdded = timeProvider.instant(),
-                order = if (orderIsAscending) (maxOrder + (i + 1) * 1000) else (minOrder - (i + 1) * 1000)
+                order = if (orderIsAscending) (maxOrder + (i + 1) * SECONDS) else (minOrder - ((newItems.size - i)) * SECONDS)
             )
         })
+        log.d(playlistWithNewItemsOnly.summarise())
+        //log.d("playlistWithNewItemsOnly: ${playlistWithNewItemsOnly.items.size}")
+        return playlistWithNewItemsOnly
     }
 
     interface UpdateCheck {
@@ -96,5 +106,6 @@ class PlaylistUpdateUsecase constructor(
 
     companion object {
         private val UPDATE_INTERVAL_DEFAULT = 1000 * 60 * 60
+        val SECONDS = 1000L
     }
 }
