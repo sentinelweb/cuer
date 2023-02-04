d31f516863269f57f68bdf8420782f74cbe0fa82
sentinelweb <sentinelwebtechnologies@gmail.com>
2023-02-04 02:11:17 +0100

    403 fix Sc tests

--- app/src/sharedTest/java/uk/co/sentinelweb/cuer/app/ui/share/scan/ScanFragmentSharedTest.kt
+++ app/src/sharedTest/java/uk/co/sentinelweb/cuer/app/ui/share/scan/ScanFragmentSharedTest.kt
@@ -5,13 +5,18 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import com.flextrade.jfixture.JFixture
+import com.appmattus.kotlinfixture.decorator.nullability.NeverNullStrategy
+import com.appmattus.kotlinfixture.decorator.nullability.nullabilityStrategy
+import com.appmattus.kotlinfixture.decorator.optional.NeverOptionalStrategy
+import com.appmattus.kotlinfixture.decorator.optional.optionalStrategy
+import com.appmattus.kotlinfixture.kotlinFixture
 import org.hamcrest.CoreMatchers
 import org.mockito.Mockito
 import org.mockito.Mockito.verify
 import uk.co.sentinelweb.cuer.app.R
-import uk.co.sentinelweb.cuer.domain.Domain
-import uk.co.sentinelweb.cuer.domain.MediaDomain
+import uk.co.sentinelweb.cuer.app.orchestrator.OrchestratorContract
+import uk.co.sentinelweb.cuer.domain.*
+import uk.co.sentinelweb.cuer.domain.creator.GuidCreator
 import uk.co.sentinelweb.cuer.tools.provider.FragmentScenarioProvider
 import uk.co.sentinelweb.cuer.tools.test.matchers.drawableMatches
 
@@ -19,16 +24,26 @@ class ScanFragmentSharedTest constructor(
     val scenario: FragmentScenarioProvider<ScanFragment>,
     val mockPresenter: ScanContract.Presenter
 ) {
-    private val fixture = JFixture()
+    private val fixture = kotlinFixture {
+        nullabilityStrategy(NeverNullStrategy)
+        repeatCount { 5 }
+        factory { OrchestratorContract.Identifier(GuidCreator().create(), fixture()) }
+        optionalStrategy(NeverOptionalStrategy) {
+            propertyOverride(PlaylistDomain::id, NeverOptionalStrategy)
+            propertyOverride(PlaylistItemDomain::id, NeverOptionalStrategy)
+            propertyOverride(MediaDomain::id, NeverOptionalStrategy)
+            propertyOverride(ImageDomain::id, NeverOptionalStrategy)
+            propertyOverride(ChannelDomain::id, NeverOptionalStrategy)
+        }
+        subType<Domain, MediaDomain>()
+    }
 
     fun setup() {
-        fixture.customise()
-            .lazyInstance(Domain::class.java, { fixture.create(MediaDomain::class.java) })
     }
 
     fun fromShareUrl() {
 
-        val fixtUrl = fixture.create(String::class.java)
+        val fixtUrl: String = fixture()
 
         scenario.get().onFragment { it.fromShareUrl(fixtUrl) }
         onView(withId(R.id.scan_progress)).check(matches(isDisplayed()))
@@ -37,7 +52,7 @@ class ScanFragmentSharedTest constructor(
     }
 
     fun setModel_isLoading() {
-        val fixtModel = fixture.create(ScanContract.Model::class.java).copy(isLoading = true)
+        val fixtModel = fixture<ScanContract.Model>().copy(isLoading = true)
         scenario.get().onFragment { it.setModel(fixtModel) }
         onView(withId(R.id.scan_progress)).check(matches(isDisplayed()))
         onView(withId(R.id.scan_result)).check(matches(CoreMatchers.not(isDisplayed())))
@@ -46,7 +61,7 @@ class ScanFragmentSharedTest constructor(
     }
 
     fun setModel_isNotLoading() {
-        val fixtModel = fixture.create(ScanContract.Model::class.java).copy(
+        val fixtModel = fixture<ScanContract.Model>().copy(
             isLoading = false,
             resultIcon = R.drawable.ic_item_tick_white
         )
@@ -59,7 +74,7 @@ class ScanFragmentSharedTest constructor(
     }
 
     fun setResult() {
-        val fixtResult = fixture.create(ScanContract.Result::class.java)
+        val fixtResult = fixture<ScanContract.Result>()
         val mockListener = Mockito.mock(ScanContract.Listener::class.java)
         scenario.get().apply {
             onFragment { it.listener = mockListener }
