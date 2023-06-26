f472c1b6d994aa691fb310e125b2eaacc3400a6d
robmunro <rrmunro@gmail.com>
2023-06-26 21:00:36 +0200

    196 migration working but not verify (new verify schema doesnt gen properly)

--- database/src/commonMain/kotlin/uk/co/sentinelweb/cuer/db/factory/DatabaseFactory.kt
+++ database/src/commonMain/kotlin/uk/co/sentinelweb/cuer/db/factory/DatabaseFactory.kt
@@ -3,6 +3,7 @@ package uk.co.sentinelweb.cuer.db.factory
 import com.squareup.sqldelight.EnumColumnAdapter
 import com.squareup.sqldelight.db.SqlDriver
 import uk.co.sentinelweb.cuer.app.db.Database
+import uk.co.sentinelweb.cuer.core.wrapper.LogWrapper
 import uk.co.sentinelweb.cuer.database.entity.Channel
 import uk.co.sentinelweb.cuer.database.entity.Media
 import uk.co.sentinelweb.cuer.database.entity.Playlist
@@ -11,7 +12,11 @@ import uk.co.sentinelweb.cuer.db.adapter.InstantAdapter
 import uk.co.sentinelweb.cuer.db.adapter.LocalDatetimeAdapter
 import uk.co.sentinelweb.cuer.db.adapter.PlaylistConfigDomainAdapter
 
-class DatabaseFactory {
+class DatabaseFactory(private val log: LogWrapper) {
+
+    init {
+        log.tag(this)
+    }
 
     fun createDatabase(driver: SqlDriver): Database {
         return Database(
@@ -36,12 +41,15 @@ class DatabaseFactory {
                 date_addedAdapter = InstantAdapter()
             )
         ).apply {
+            log.d("Database.Schema.version ${Database.Schema.version}")
+        }.apply {
             Database.Schema.migrate(
                 driver = driver,
-                oldVersion = 1,
+                oldVersion = Database.Schema.version,
                 newVersion = 2,
             )
+        }.apply {
+            log.d("Database.Schema.version ${Database.Schema.version}")
         }
-
     }
 }
