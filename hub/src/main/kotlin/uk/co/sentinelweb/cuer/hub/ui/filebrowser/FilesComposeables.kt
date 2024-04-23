3e463cec4d95ae7f7378c0a11070402c60f42498
robmunro <rrmunro@gmail.com>
2024-04-23 23:47:50 +0100

    131 add icons to file browser

--- hub/src/main/kotlin/uk/co/sentinelweb/cuer/hub/ui/filebrowser/FilesComposeables.kt
+++ hub/src/main/kotlin/uk/co/sentinelweb/cuer/hub/ui/filebrowser/FilesComposeables.kt
@@ -2,12 +2,15 @@ package uk.co.sentinelweb.cuer.hub.ui.filebrowser
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import uk.co.sentinelweb.cuer.domain.MediaDomain
+import uk.co.sentinelweb.cuer.hub.ui.common.image.ImageSvg
 
 object FilesComposeables {
     @Composable
@@ -18,13 +21,25 @@ object FilesComposeables {
 
     @Composable
     private fun FilesView(model: FilesModel, view: FilesUiCoordinator) {
-
         Column(modifier = Modifier.padding(8.dp)) {
             model.list.subPlaylists.forEach {
-                Text(text = it.title, modifier = Modifier.padding(16.dp).clickable { view.loadFolder(it) })
+                Row(modifier = Modifier.clickable { view.loadFolder(it) }) {
+                    val icon = if (it.title.equals("..")) "drawable/ic_up.svg" else "drawable/ic_folder.svg"
+                    ImageSvg(icon, modifier = Modifier.padding(16.dp))
+                    Text(text = it.title, modifier = Modifier.padding(16.dp))
+                }
             }
             model.list.items.forEach {
-                Text(text = it.media.title?:"No title", modifier = Modifier.padding(16.dp))
+                Row {
+                    val icon = when (it.media.mediaType) {
+                        MediaDomain.MediaTypeDomain.VIDEO -> "drawable/ic_video.svg"
+                        MediaDomain.MediaTypeDomain.AUDIO -> "drawable/ic_mic.svg"
+                        MediaDomain.MediaTypeDomain.WEB -> "drawable/ic_browse.svg"
+                        MediaDomain.MediaTypeDomain.FILE -> "drawable/ic_file.svg"
+                    }
+                    ImageSvg(icon, modifier = Modifier.padding(16.dp))
+                    Text(text = it.media.title ?: "No title", modifier = Modifier.padding(16.dp))
+                }
             }
         }
     }
