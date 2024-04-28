28535f1e42a93aa1619d689fb5ba4bf53c369027
robmunro <rrmunro@gmail.com>
2024-04-28 02:11:21 +0100

    467 extract vlcj ver

--- hub/build.gradle.kts
+++ hub/build.gradle.kts
@@ -14,7 +14,7 @@ val ver_junit: String by project
 val ver_mockk: String by project
 val ver_turbine: String by project
 val ver_multiplatform_settings: String by project
-
+val ver_vlcj: String by project
 val isProduction: String by project
 val app_versionCode: String by project
 val app_versionName: String by project
@@ -60,7 +60,7 @@ dependencies {
     testImplementation("app.cash.turbine:turbine:$ver_turbine")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$ver_coroutines")
 
-    implementation("uk.co.caprica:vlcj:4.8.2")
+    implementation("uk.co.caprica:vlcj:$ver_vlcj")
 
 }
 
