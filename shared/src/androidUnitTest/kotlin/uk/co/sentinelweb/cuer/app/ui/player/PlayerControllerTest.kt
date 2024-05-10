cf94ca8397c23bbfc3db2bcdff419d61b69ee59f
rob_munro <rrmunro@gmail.com>
2024-05-10 10:11:10 +0100

    131 fix PlayerControllerTest

--- shared/src/androidUnitTest/kotlin/uk/co/sentinelweb/cuer/app/ui/player/PlayerControllerTest.kt
+++ shared/src/androidUnitTest/kotlin/uk/co/sentinelweb/cuer/app/ui/player/PlayerControllerTest.kt
@@ -12,6 +12,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import uk.co.sentinelweb.cuer.app.queue.QueueMediatorContract
+import uk.co.sentinelweb.cuer.app.service.remote.player.PlayerSessionListener
 import uk.co.sentinelweb.cuer.app.ui.player.PlayerContract.MviStore.Intent.SeekToPosition
 import uk.co.sentinelweb.cuer.core.providers.TestCoroutineContextProvider
 import uk.co.sentinelweb.cuer.core.wrapper.LogWrapper
@@ -24,30 +25,50 @@ class PlayerControllerTest {
 
     private val queueConsumer: QueueMediatorContract.Consumer = mockk(relaxed = true)
     private val modelMapper: PlayerModelMapper = mockk(relaxed = true)
-    private val playControls: MediaSessionListener = mockk(relaxed = true)
+    private val mediaSessionListener: MediaSessionListener = mockk(relaxed = true)
     private val log: LogWrapper = mockk(relaxed = true)
     private val store: PlayerContract.MviStore = mockk(relaxed = true)
     private val testCoroutines = TestCoroutineContextProvider(rule.dispatcher)
+    private val playerSessionListener: PlayerSessionListener = mockk(relaxed = true)
 
     private lateinit var sut: PlayerController
 
     @Before
     fun setUp() {
         sut = PlayerController(
-            queueConsumer, modelMapper, testCoroutines,
-            playControls, log, store, null
+            queueConsumer = queueConsumer,
+            modelMapper = modelMapper,
+            coroutines = testCoroutines,
+            mediaSessionListener = mediaSessionListener,
+            playSessionListener = playerSessionListener,
+            log = log,
+            store = store,
+            lifecycle = null,
         )
     }
 
     @Test
-    fun onViewCreated() = runTest {
+    fun onViewCreated_mediaSessionFlow() = runTest {
         val slot = slot<PlayerContract.MviStore.Intent>()
-        val mutableSharedFlow = MutableSharedFlow<PlayerContract.MviStore.Intent>()
-        coEvery { playControls.intentFlow } returns mutableSharedFlow
+        val mediaMutableSharedFlow = MutableSharedFlow<PlayerContract.MviStore.Intent>()
+        coEvery { mediaSessionListener.intentFlow } returns mediaMutableSharedFlow
         sut.onViewCreated(listOf())
         sut.onStart()
         val expected = SeekToPosition(1000)
-        mutableSharedFlow.emit(expected)
+        mediaMutableSharedFlow.emit(expected)
+        coVerify { store.accept(capture(slot)) }
+        Truth.assertThat(slot.captured).isEqualTo(expected)
+    }
+
+    @Test
+    fun onViewCreated_playerSessionFlow() = runTest {
+        val slot = slot<PlayerContract.MviStore.Intent>()
+        val playerMutableSharedFlow = MutableSharedFlow<PlayerContract.MviStore.Intent>()
+        coEvery { playerSessionListener.intentFlow } returns playerMutableSharedFlow
+        sut.onViewCreated(listOf())
+        sut.onStart()
+        val expected = SeekToPosition(1000)
+        playerMutableSharedFlow.emit(expected)
         coVerify { store.accept(capture(slot)) }
         Truth.assertThat(slot.captured).isEqualTo(expected)
     }
