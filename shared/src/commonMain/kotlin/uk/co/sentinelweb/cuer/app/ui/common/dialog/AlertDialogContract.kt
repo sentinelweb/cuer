7b207ff6409648ee0e9cade73549e6db7c852c68
sentinelweb <robmunro@robs-MacBook-Pro.local>
2024-09-01 18:39:06 +0200

    457 refactor play dialog for fragment leak.

--- shared/src/commonMain/kotlin/uk/co/sentinelweb/cuer/app/ui/common/dialog/AlertDialogContract.kt
+++ shared/src/commonMain/kotlin/uk/co/sentinelweb/cuer/app/ui/common/dialog/AlertDialogContract.kt
@@ -2,7 +2,7 @@ package uk.co.sentinelweb.cuer.app.ui.common.dialog
 
 interface AlertDialogContract {
     interface Creator {
-        fun create(model: AlertDialogModel): Any
+        fun create(model: AlertDialogModel): Any /* dialog ref */
 
         fun createAndShowDialog(model: AlertDialogModel)
 
